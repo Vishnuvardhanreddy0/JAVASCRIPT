8b9dcec33fa749075cab29f8e1d3b378e609d1ee
Vishnu vardhan reddy <160316524+Vishnuvardhanreddy0@users.noreply.github.com>
2024-03-14 20:34:21 +0530

    Add files via upload

---
+++ JS/one.js
@@ -0,0 +1,28 @@
+var eid='101';
+var ename="Vardhan";
+var _esalary=45000;
+var $product_name="Xiomi11i";
+var product_price=30000;
+var product_rating=4.5;
+var loc;
+var product_delivery=true
+var product_company=["flipkart:laptop","amazon:iphone"]
+
+console.log(eid)
+console.log(typeof eid)
+console.log(ename)
+console.log(typeof ename)
+console.log(_esalary)
+console.log(typeof _esalary)
+console.log($product_name)
+console.log(typeof $product_name)
+console.log(product_price)
+console.log(typeof product_price)
+console.log(product_rating)
+console.log(typeof product_rating)
+console.log(loc)
+console.log(typeof loc)
+console.log(product_delivery)
+console.log(typeof product_delivery)
+console.log(product_company)
+console.log(typeof product_company)
